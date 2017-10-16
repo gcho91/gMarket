377873f64960739f3fbfc86c413d91ad217aba94
gcho91 <gcho91@gmail.com>
2017-10-16 00:31:52 -0500

    Product ID page showing on details/:id but not shop/:id

--- public/js/app.js
+++ public/js/app.js
@@ -14,14 +14,16 @@ angular.module('gMarket', ['ui.router']).config(function($stateProvider, $urlRou
   //show by product type
   .state('shopbytype', {
     url: "/shop/:type",
+    // url: "/shop/type",
     templateUrl: "views/shopbytype.html",
     controller: "shopbytypeCtrl"
   })
   //show product details by id
   .state('details', {
 
-    // url: '/details/:id',
-    url: '/shop/:id',
+    url: '/details/:id',
+    // url: '/shop/:id',
+    //what i want is /shop/:id but only details/:id is working...
     templateUrl: "views/details.html",
     controller: "detailsCtrl"
   })
--- public/js/controllers/detailsCtrl.js
+++ public/js/controllers/detailsCtrl.js
@@ -1,9 +1,14 @@
 angular.module('gMarket').controller('detailsCtrl', function($scope, $stateParams, shopService) {
 
   console.log($stateParams.id);
-  shopService.getProductById($stateParams.id).then(function(response) {
-    console.log(response[0])
+
+ shopService.getProductById($stateParams.id).then(function(response) {
+    // console.log(response[0])
+    console.log(response)
     $scope.product = response[0];
+
+    // console.log($scope.random)
+    //$scope.random/dtails just makes it available to the view???
 })
 
 });
